276ff18b7866352aafe3546e4ee9e9410cf88de1
canh18377 <canh10101101@gmail.com>
2024-11-26 16:06:54 +0700

    root

---
+++ QlNhanSu/Views/FormMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace QlNhanSu.Views
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.qLNhanSuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.themMenuVaoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.qLNhanSuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // qLNhanSuToolStripMenuItem
+            // 
+            this.qLNhanSuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.themMenuVaoToolStripMenuItem});
+            this.qLNhanSuToolStripMenuItem.Name = "qLNhanSuToolStripMenuItem";
+            this.qLNhanSuToolStripMenuItem.Size = new System.Drawing.Size(112, 29);
+            this.qLNhanSuToolStripMenuItem.Text = "QLNhanSu";
+            // 
+            // themMenuVaoToolStripMenuItem
+            // 
+            this.themMenuVaoToolStripMenuItem.Name = "themMenuVaoToolStripMenuItem";
+            this.themMenuVaoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.themMenuVaoToolStripMenuItem.Text = "Them Menu vao";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem qLNhanSuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem themMenuVaoToolStripMenuItem;
+    }
+}
