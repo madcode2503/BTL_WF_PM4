a3305ece99b99ada557c4b7070b4b8ff48ce9de0
canh18377 <canh10101101@gmail.com>
2024-11-27 12:32:59 +0700

    root

---
+++ QLNhanSu/Models/EnumErrcode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNhanSu.Models
+{
+    internal enum EnumErrcode
+    {
+        Error = -1,
+        Empty = 0,
+        Success = 1
+    }
+}
---
+++ QLNhanSu/Models/FunctionResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNhanSu.Models
+{
+    internal class FunctionResult<T>
+    {
+        public EnumErrcode ErrCode { get; set; }//success/fail
+        public string ErrDesc { get; set; }
+        public T Data { get; set; }
+    }
+}
